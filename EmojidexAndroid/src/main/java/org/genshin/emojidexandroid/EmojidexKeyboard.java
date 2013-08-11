4f5e478dd5d8034a748052b615691f5e155282e3
kou <kou@genshin.org>
2013-08-11 22:55:57 +0900

    Create EmojidexKeyboard.java file.

---
+++ EmojidexAndroid/src/main/java/org/genshin/emojidexandroid/EmojidexKeyboard.java
@@ -0,0 +1,52 @@
+package org.genshin.emojidexandroid;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+
+/**
+ * Created by kou on 13/08/11.
+ */
+public class EmojidexKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
