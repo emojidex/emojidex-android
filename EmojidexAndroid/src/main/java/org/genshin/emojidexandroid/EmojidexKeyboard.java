adab0f02b3b0a7893b96c6f5a84b015d8fa40eee
kou <kou@genshin.org>
2013-08-23 16:30:49 +0900

    Create keyboard layout.

--- EmojidexAndroid/src/main/java/org/genshin/emojidexandroid/EmojidexKeyboard.java
+++ EmojidexAndroid/src/main/java/org/genshin/emojidexandroid/EmojidexKeyboard.java
@@ -3,13 +3,31 @@ package org.genshin.emojidexandroid;
 import android.app.Service;
 import android.content.Intent;
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.IBinder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Created by kou on 13/08/11.
  */
 public class EmojidexKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    View layout;
+
+    @Override
+    public void onInitializeInterface() {
+        super.onInitializeInterface();
+    }
+
+    @Override
+    public View onCreateInputView() {
+        layout = (View)getLayoutInflater().inflate(R.layout.keyboard, null);
+        return layout;
+    }
+
     @Override
     public void onPress(int i) {
 
