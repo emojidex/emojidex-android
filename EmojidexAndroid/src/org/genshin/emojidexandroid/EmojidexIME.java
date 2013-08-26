e34879a8e18de560493367073d0bf03ddbd1f192
kou <kou@genshin.org>
2013-08-26 16:20:55 +0900

    Created empty keyboard.

--- EmojidexAndroid/src/org/genshin/emojidexandroid/EmojidexIME.java
+++ EmojidexAndroid/src/org/genshin/emojidexandroid/EmojidexIME.java
@@ -1,23 +1,37 @@
 package org.genshin.emojidexandroid;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by kou on 13/08/11.
  */
 public class EmojidexIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    Keyboard keyboard;
     View layout;
 
     @Override
     public void onInitializeInterface() {
-        super.onInitializeInterface();
+        keyboard = new Keyboard(this, R.xml.keyboard);
     }
 
     @Override
     public View onCreateInputView() {
+        // load IME layout from xml.
         layout = (View)getLayoutInflater().inflate(R.layout.ime, null);
+
+        // create keyboard view.
+        KeyboardView keyboardView = new KeyboardView(this, null);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+
+        // add keyboard view to IME layout.
+        ViewGroup targetView = (ViewGroup)layout.findViewById(R.id.ime_keyboard);
+        targetView.addView(keyboardView);
+
         return layout;
     }
 
