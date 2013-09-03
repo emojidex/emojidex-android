9f219c14dcab15fb160d2151933d430d8c7838d2
kou <kou@genshin.org>
2013-09-03 17:22:09 +0900

    Create keyboard from "index.json".

--- EmojidexAndroid/src/org/genshin/emojidexandroid/MainActivity.java
+++ EmojidexAndroid/src/org/genshin/emojidexandroid/MainActivity.java
@@ -1,7 +1,7 @@
 package org.genshin.emojidexandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
