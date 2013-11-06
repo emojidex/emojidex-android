f4b8f4953fa45fb83623ba329882a615944ddb3b
Rika Yoshida <nazuki@genshin.org>
2013-11-06 13:34:17 +0900

    Implementing a function to convert when the flick

--- EmojidexAndroid/src/org/genshin/emojidexandroid/MainActivity.java
+++ EmojidexAndroid/src/org/genshin/emojidexandroid/MainActivity.java
@@ -12,14 +12,14 @@ import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
-    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
 
-        initEmojidexTest();
+        initEmojidexEditor();
+        //initEmojidexTest();
     }
 
     @Override
@@ -37,6 +37,12 @@ public class MainActivity extends Activity {
     private Emojidex emojidex = null;
     private EditText testEditText = null;
 
+    private EditText emojiEditText;
+    private EditText textEditText;
+    private EditText emojiHalfEditText;
+    private EditText textHalfEditText;
+    private ViewFlipper viewFlipper;
+
     public void emojifyTest(View v)
     {
         final CharSequence src = testEditText.getText();
@@ -53,13 +59,14 @@ public class MainActivity extends Activity {
         testEditText.setText(dest);
     }
 
+    /*
     public void actionSendTest(View v)
     {
         try
         {
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.putExtra(Intent.EXTRA_TEXT, testEditText.getText());
-            intent.setType("text_view/plain");
+            intent.setType("text/plain");
             startActivity(Intent.createChooser(intent, "Send to"));
         }
         catch(Exception e)
@@ -68,7 +75,9 @@ public class MainActivity extends Activity {
             Log.d("hoge", "send error.");
         }
     }
+    */
 
+    /*
     private void initEmojidexTest()
     {
         if(emojidex == null)
@@ -80,6 +89,75 @@ public class MainActivity extends Activity {
         viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper_editor);
         viewFlipper.setOnTouchListener(new FlickTouchListener());
         viewFlipper.showNext();
+    }
+    */
+
+    private void initEmojidexEditor()
+    {
+        if (emojidex == null)
+            emojidex = new Emojidex(getApplicationContext());
+
+        emojiEditText = (EditText)findViewById(R.id.emoji_edittext);
+        textEditText = (EditText) findViewById(R.id.text_edittext);
+        emojiHalfEditText = (EditText)findViewById(R.id.emoji_half_edittext);
+        textHalfEditText = (EditText)findViewById(R.id.text_half_edittext);
+
+        // set viewFlipper action
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper_editor);
+        viewFlipper.setOnTouchListener(new FlickTouchListener());
+        // set default view
+        viewFlipper.showNext();
+    }
+
+    private CharSequence emojify(CharSequence cs)
+    {
+        final CharSequence src = cs;
+        final CharSequence dest = emojidex.emojify(src);
+
+        return dest;
+    }
+
+    private CharSequence deEmojify(CharSequence cs)
+    {
+        final CharSequence src = cs;
+        final CharSequence dest = emojidex.deEmojify(src);
+
+        return dest;
+    }
+
+    public void share(View v)
+    {
+        try
+        {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            if (viewFlipper.getCurrentView() == findViewById(R.id.emoji_layout))
+                intent.putExtra(Intent.EXTRA_TEXT, emojiEditText.getText());
+            else if (viewFlipper.getCurrentView() == findViewById(R.id.text_layout))
+                intent.putExtra(Intent.EXTRA_TEXT, textEditText.getText());
+
+            intent.setType("text/plain");
+            startActivity(Intent.createChooser(intent, "Send to"));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            Log.d("hoge", "send error.");
+        }
+    }
+
+    public void moveRight(View v)
+    {
+        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_in));
+        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_out));
+        viewFlipper.showNext();
+    }
+
+    public void moveLeft(View v)
+    {
+        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_in));
+        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_out));
+        viewFlipper.showPrevious();
     }
 
     private float lastTouchX;
@@ -99,12 +177,29 @@ public class MainActivity extends Activity {
                     currentX = event.getX();
                     if (lastTouchX < currentX && viewFlipper.getCurrentView() != findViewById(R.id.emoji_text_layout))
                     {
+                        // move text and emojify
+                        if (viewFlipper.getCurrentView() == findViewById(R.id.text_layout))
+                            emojiEditText.setText(emojify(textEditText.getText()));
+                        // move emoji and deEmojify
+                        else
+                        {
+                            emojiHalfEditText.setText(emojiEditText.getText());
+                            textHalfEditText.setText(deEmojify(emojiEditText.getText()));
+                        }
+
                         viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_in));
                         viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_out));
                         viewFlipper.showPrevious();
                     }
                     if (lastTouchX > currentX && viewFlipper.getCurrentView() != findViewById(R.id.text_layout))
                     {
+                        // move emoji
+                        if (viewFlipper.getCurrentView() == findViewById(R.id.emoji_text_layout))
+                            emojiEditText.setText(emojiHalfEditText.getText());
+                        // move text and deEmojify
+                        else
+                            textEditText.setText(deEmojify(emojiEditText.getText()));
+
                         viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_in));
                         viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_out));
                         viewFlipper.showNext();
