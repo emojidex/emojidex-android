a715c3a01dccf1248254adde72f8e735b3e26d45
Rika Yoshida <nazuki@genshin.org>
2014-01-16 12:25:40 +0900

    comment out the location of the work

--- EmojidexAndroid/src/org/genshin/emojidexandroid/EmojidexIME.java
+++ EmojidexAndroid/src/org/genshin/emojidexandroid/EmojidexIME.java
@@ -295,7 +295,7 @@ public class EmojidexIME extends InputMethodService implements KeyboardView.OnKe
         keyboardViewFlipper.removeAllViews();
         for (int i = 0; i < categorizedKeyboards.get(categoryID).getKeyboards().size(); i++)
         {
-            EmojidexKeyboardView keyboardView = new EmojidexKeyboardView(this, null, R.attr.keyboardViewStyle);
+            KeyboardView keyboardView = new KeyboardView(this, null, R.attr.keyboardViewStyle);
             keyboardView.setOnKeyboardActionListener(this);
             keyboardView.setPreviewEnabled(false);
             keyboardView.setKeyboard(categorizedKeyboards.get(categoryID).getKeyboards().get(i));
