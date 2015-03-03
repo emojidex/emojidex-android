ee3e67f3d233d66716c897a4ea70b3c9e9e21746
kou <kou@genshin.org>
2015-03-03 15:43:33 +0900

    Remove trush and optimize imports.

--- EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/EmojidexIME.java
+++ EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/EmojidexIME.java
@@ -11,7 +11,6 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.GestureDetector;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
