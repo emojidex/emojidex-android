ca3fb1a378a4c863d7996d0ff4847e14d7766d40
Rika Yoshida <nazuki@genshin.org>
2015-09-02 12:05:01 +0900

    add tutorial

--- EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/TutorialActivity.java
+++ EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/TutorialActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -19,7 +21,6 @@ public class TutorialActivity extends Activity {
 
     private ImageButton nextButton;
     private ImageButton prevButton;
-    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +31,22 @@ public class TutorialActivity extends Activity {
     }
 
     private void createTutorial() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         page = 0;
 
         nextButton = (ImageButton)findViewById(R.id.tutotial_next_button);
         prevButton = (ImageButton)findViewById(R.id.tutotial_prev_button);
         prevButton.setVisibility(View.INVISIBLE);
 
-        // TODO: 英語版の時の画像
+        // TODO: 英語版の時は？
         viewFlipper = (ViewFlipper)findViewById(R.id.tutorial_view_flipper);
         for (int i = 1; i <= 8; i++) {
             ImageView imageView = new ImageView(getApplicationContext());
 
-            if (i == 1) {
-                int resourceId = getResources().getIdentifier("tutorial_01", "drawable", getPackageName());
-                bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+            if (i <= 2) {
+                int resourceId = getResources().getIdentifier("tutorial_0" + i, "drawable", getPackageName());
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
                 imageView.setImageBitmap(bitmap);
             }
 
@@ -58,7 +61,6 @@ public class TutorialActivity extends Activity {
 
             @Override
             public void onShowPress(MotionEvent e) {
-
             }
 
             @Override
@@ -73,14 +75,12 @@ public class TutorialActivity extends Activity {
 
             @Override
             public void onLongPress(MotionEvent e) {
-
             }
 
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 float disX = e1.getX() - e2.getX();
-                if (Math.abs(disX) < 100)
-                    return true;
+                if (Math.abs(disX) < 100) return true;
 
                 if (disX > 0) {
                     tutorialShowNext(null);
@@ -94,39 +94,63 @@ public class TutorialActivity extends Activity {
     }
 
     public void tutorialShowNext(View v) {
-        if (page < 7) {
-            page++;
-            setImage();
-            setButtonVisibility();
+        if (page == 7) return;
 
-            viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_in));
-            viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_out));
-            viewFlipper.showNext();
+        page++;
+        setImage();
+        setButtonVisibility();
 
-            ((ImageView)viewFlipper.getChildAt(page - 1)).setImageBitmap(null);
-        }
+        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_in));
+        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_out));
+        viewFlipper.setLayoutAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                releaseImage((ImageView) viewFlipper.getChildAt(page - 1));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        viewFlipper.showNext();
     }
 
     public void tutorialShowPrevious(View v) {
-        if (page > 0) {
-            page--;
-            setImage();
-            setButtonVisibility();
+        if (page == 0) return;
 
-            viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_in));
-            viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_out));
-            viewFlipper.showPrevious();
+        page--;
+        setImage();
+        setButtonVisibility();
 
-            ((ImageView) viewFlipper.getChildAt(page + 1)).setImageBitmap(null);
-        }
+        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_in));
+        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_out));
+        viewFlipper.setLayoutAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                releaseImage((ImageView) viewFlipper.getChildAt(page + 1));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        viewFlipper.showPrevious();
     }
 
     private void setImage() {
-        recycleBitmap();
-
         ImageView imageView = (ImageView)viewFlipper.getChildAt(page);
+        releaseImage(imageView);
+
         int resourceId = getResources().getIdentifier("tutorial_0" + (page + 1), "drawable", getPackageName());
-        bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
         imageView.setImageBitmap(bitmap);
     }
 
@@ -144,22 +168,27 @@ public class TutorialActivity extends Activity {
     }
 
     public void closeTutorial(View v) {
-        recycleBitmap();
+        releaseImages();
 
-        // TODO: MainActivityに戻る
+        finish();
     }
 
-    private void recycleBitmap() {
-        if (bitmap != null) {
-            bitmap.recycle();
-            bitmap = null;
+    private void releaseImage(ImageView imageView) {
+        imageView.setImageBitmap(null);
+        imageView.setImageDrawable(null);
+    }
+
+    private void releaseImages() {
+        for (int i = 0; i < 8; i++) {
+            ImageView imageView = (ImageView) viewFlipper.getChildAt(i);
+            releaseImage(imageView);
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        recycleBitmap();
+        releaseImages();
     }
 
     @Override
