e18343433e0923efbe2ef26efb146f79ebd843db
kou <kou@genshin.org>
2016-06-20 15:56:52 +0900

    Check usagestats when android 6.

--- EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/EmojidexKeyboardView.java
+++ EmojidexAndroid/src/main/java/com/emojidex/emojidexandroid/EmojidexKeyboardView.java
@@ -1,27 +1,36 @@
 package com.emojidex.emojidexandroid;
 
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.AppOpsManager;
 import android.app.usage.UsageEvents;
 import android.app.usage.UsageStatsManager;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
+import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Build;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
@@ -99,145 +108,228 @@ public class EmojidexKeyboardView extends KeyboardView {
     {
         closePopup();
 
-        // Create popup window.
-        View view = inflater.inflate(R.layout.popup_favorite, null);
-        popup = new PopupWindow(this);
-        popup.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.background_blank));
-        popup.setOutsideTouchable(true);
-        popup.setFocusable(false);
-        popup.setContentView(view);
-        popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
-        popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-        popup.setOnDismissListener(new PopupWindow.OnDismissListener()
+        final Runnable showPopup = new Runnable()
         {
             @Override
-            public void onDismiss()
+            public void run()
             {
-                // If changed favorite state, save current state.
-                if (registered != first)
+                final View parent = EmojidexKeyboardView.this;
+
+                // Create popup window.
+                View view = inflater.inflate(R.layout.popup_favorite, null);
+                popup = new PopupWindow(parent);
+                popup.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.background_blank));
+                popup.setOutsideTouchable(true);
+                popup.setFocusable(false);
+                popup.setContentView(view);
+                popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+                popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+                popup.setOnDismissListener(new PopupWindow.OnDismissListener()
                 {
-                    if (registered)
-                        favoriteManager.addFirst(emojiName);
-                    else
-                        favoriteManager.remove(emojiName);
+                    @Override
+                    public void onDismiss()
+                    {
+                        // If changed favorite state, save current state.
+                        if (registered != first)
+                        {
+                            if (registered)
+                                favoriteManager.addFirst(emojiName);
+                            else
+                                favoriteManager.remove(emojiName);
+                        }
+                    }
+                });
+                popup.showAtLocation(parent, Gravity.CENTER, 0, -parent.getHeight());
+
+                // Set emoji data.
+                popupTextView = (TextView)view.findViewById(R.id.favorite_name);
+                popupTextView.setText(":" + key.popupCharacters + ":");
+                popupTextView.setOnLongClickListener(new OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        ClipboardManager manager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData.Item item = new ClipData.Item(":" + key.popupCharacters + ":");
+                        String[] mimeType = new String[1];
+                        mimeType[0] = ClipDescription.MIMETYPE_TEXT_PLAIN;
+                        ClipData data = new ClipData(new ClipDescription("text_data", mimeType), item);
+                        manager.setPrimaryClip(data);
+                        Toast.makeText(context, R.string.clipboard, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popupIcon = (ImageView)view.findViewById(R.id.popup_favorite_image);
+                popupIcon.setImageDrawable(key.icon);
+
+                // Set register button.
+                imageButton = (ImageButton)view.findViewById(R.id.favorite_register_button);
+                imageButton.setOnClickListener(createListener());
+
+                // Set star(favorite) icon from current state.
+                setCurrentState();
+
+                // Set close button.
+                Button closeButton = (Button)view.findViewById(R.id.popup_close_button);
+                closeButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        closePopup();
+                    }
+                });
+
+                // Set seal send button.
+                final ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+                final Button sealSendButton = (Button)view.findViewById(R.id.popup_seal_send_button);
+
+                String packageName;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    final LollipopTaskManager ltm = LollipopTaskManager.getInstance(context);
+                    packageName = ltm.getCurrentActivity();
+                }
+                else
+                {
+                    packageName = am.getRunningTasks(1).get(0).baseActivity.getPackageName();
+                }
+                final String targetPackageName = packageName;
+
+                final Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("image/png");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                intent.setPackage(targetPackageName);
+
+                if(context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty())
+                {
+                    sealSendButton.setVisibility(GONE);
+                }
+                else
+                {
+                    sealSendButton.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            closePopup();
+
+                            // Send intent.
+                            final Intent proxyIntent = new Intent(context, SendSealActivity.class);
+                            proxyIntent.putExtra(SendSealActivity.EXTRA_EMOJI_NAME, emojiName);
+                            proxyIntent.putExtra(SendSealActivity.EXTRA_PACKAGE_NAME, targetPackageName);
+                            proxyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            context.startActivity(proxyIntent);
+                        }
+                    });
+                }
+
+                // Set variants.
+                final Context viewContext = view.getContext();
+                final FrameLayout variantsMain = (FrameLayout)view.findViewById(R.id.favorite_variants_main);
+                variantsScrollView = new CustomHorizontalScrollView(viewContext);
+                variantsLayout = new CustomLinearLayout(viewContext);
+                variantsLayout.setOrientation(LinearLayout.HORIZONTAL);
+                variantsScrollView.addView(variantsLayout);
+                variantsMain.addView(variantsScrollView);
+
+                variantsLayoutArea = (LinearLayout)view.findViewById(R.id.favorite_variants_area);
+
+                final int repeatInterval = 100;
+                final int scrollSpeed = 500 * repeatInterval / 1000;
+
+                variantsArrowLeft = view.findViewById(R.id.favorite_variants_button_left);
+                new RepeatClickHelper(variantsArrowLeft, repeatInterval);
+                variantsArrowLeft.setVisibility(INVISIBLE);
+                variantsArrowLeft.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        variantsScrollView.smoothScrollBy(-scrollSpeed, 0);
+                    }
+                });
+
+                variantsArrowRight = view.findViewById(R.id.favorite_variants_button_right);
+                new RepeatClickHelper(variantsArrowRight, repeatInterval);
+                variantsArrowRight.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        variantsScrollView.smoothScrollBy(scrollSpeed, 0);
+                    }
+                });
+
+                setVariants();
+            }
+        };
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+            final String keyIgnore = context.getString(R.string.preference_key_usagestats_check_ignore);
+            final boolean ignore = prefs.getBoolean(keyIgnore, false);
+
+            if( !ignore )
+            {
+                final AppOpsManager aom = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
+                final int uid = android.os.Process.myUid();
+                final int mode = aom.checkOp(AppOpsManager.OPSTR_GET_USAGE_STATS, uid, context.getPackageName());
+                boolean result = (mode == AppOpsManager.MODE_ALLOWED);
+                if(mode == AppOpsManager.MODE_DEFAULT)
+                {
+                    result = context.checkPermission("android.permission.PACKAGE_USAGE_STATS", android.os.Process.myPid(), uid) == PackageManager.PERMISSION_GRANTED;
+                }
+
+                if(!result)
+                {
+                    final View dialogView = LayoutInflater.from(context).inflate(R.layout.usagestats_dialog, null);
+                    final CheckBox ignoreCheckbox = (CheckBox)dialogView.findViewById(R.id.ignoreCheckBox);
+
+                    final PopupWindow dialog = new PopupWindow(this);
+                    dialog.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.background_blank));
+                    dialog.setOutsideTouchable(true);
+                    dialog.setFocusable(true);
+                    dialog.setContentView(dialogView);
+                    dialog.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+                    dialog.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+                    dialog.setOnDismissListener(new PopupWindow.OnDismissListener()
+                    {
+                        @Override
+                        public void onDismiss()
+                        {
+                            // Update preference.
+                            prefs.edit().putBoolean(keyIgnore, ignoreCheckbox.isChecked()).commit();
+                        }
+                    });
+
+                    dialogView.findViewById(R.id.negativeButton).setOnClickListener(new OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            dialog.dismiss();
+                            showPopup.run();
+                        }
+                    });
+
+                    dialogView.findViewById(R.id.positiveButton).setOnClickListener(new OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            dialog.dismiss();
+                            if(EmojidexIME.currentInstance != null)
+                                EmojidexIME.currentInstance.hideWindow();
+                            context.startActivity(
+                                    new Intent("android.settings.USAGE_ACCESS_SETTINGS")
+                                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            );
+                        }
+                    });
+
+                    popup = dialog;
+                    dialog.showAtLocation(this, Gravity.CENTER, 0, -this.getHeight());
+
+                    return;
                 }
             }
-        });
-        popup.showAtLocation(this, Gravity.CENTER, 0, -this.getHeight());
-
-        // Set emoji data.
-        popupTextView = (TextView)view.findViewById(R.id.favorite_name);
-        popupTextView.setText(":" + key.popupCharacters + ":");
-        popupTextView.setOnLongClickListener(new OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                ClipboardManager manager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData.Item item = new ClipData.Item(":" + key.popupCharacters + ":");
-                String[] mimeType = new String[1];
-                mimeType[0] = ClipDescription.MIMETYPE_TEXT_PLAIN;
-                ClipData data = new ClipData(new ClipDescription("text_data", mimeType), item);
-                manager.setPrimaryClip(data);
-                Toast.makeText(context, R.string.clipboard, Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        popupIcon = (ImageView)view.findViewById(R.id.popup_favorite_image);
-        popupIcon.setImageDrawable(key.icon);
-
-        // Set register button.
-        imageButton = (ImageButton)view.findViewById(R.id.favorite_register_button);
-        imageButton.setOnClickListener(createListener());
-
-        // Set star(favorite) icon from current state.
-        setCurrentState();
-
-        // Set close button.
-        Button closeButton = (Button)view.findViewById(R.id.popup_close_button);
-        closeButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                closePopup();
-            }
-        });
-
-        // Set seal send button.
-        final ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-        final Button sealSendButton = (Button)view.findViewById(R.id.popup_seal_send_button);
-
-        String packageName;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-        {
-            final LollipopTaskManager ltm = LollipopTaskManager.getInstance(context);
-            packageName = ltm.getCurrentActivity();
-        }
-        else
-        {
-            packageName = am.getRunningTasks(1).get(0).baseActivity.getPackageName();
-        }
-        final String targetPackageName = packageName;
-
-        final Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("image/png");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        intent.setPackage(targetPackageName);
-
-        if(context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty())
-        {
-            sealSendButton.setVisibility(GONE);
-        }
-        else
-        {
-            sealSendButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    closePopup();
-
-                    // Send intent.
-                    final Intent proxyIntent = new Intent(context, SendSealActivity.class);
-                    proxyIntent.putExtra(SendSealActivity.EXTRA_EMOJI_NAME, emojiName);
-                    proxyIntent.putExtra(SendSealActivity.EXTRA_PACKAGE_NAME, targetPackageName);
-                    proxyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                    context.startActivity(proxyIntent);
-                }
-            });
         }
 
-        // Set variants.
-        final Context viewContext = view.getContext();
-        final FrameLayout variantsMain = (FrameLayout)view.findViewById(R.id.favorite_variants_main);
-        variantsScrollView = new CustomHorizontalScrollView(viewContext);
-        variantsLayout = new CustomLinearLayout(viewContext);
-        variantsLayout.setOrientation(LinearLayout.HORIZONTAL);
-        variantsScrollView.addView(variantsLayout);
-        variantsMain.addView(variantsScrollView);
-
-        variantsLayoutArea = (LinearLayout)view.findViewById(R.id.favorite_variants_area);
-
-        final int repeatInterval = 100;
-        final int scrollSpeed = 500 * repeatInterval / 1000;
-
-        variantsArrowLeft = view.findViewById(R.id.favorite_variants_button_left);
-        new RepeatClickHelper(variantsArrowLeft, repeatInterval);
-        variantsArrowLeft.setVisibility(INVISIBLE);
-        variantsArrowLeft.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                variantsScrollView.smoothScrollBy(-scrollSpeed, 0);
-            }
-        });
-
-        variantsArrowRight = view.findViewById(R.id.favorite_variants_button_right);
-        new RepeatClickHelper(variantsArrowRight, repeatInterval);
-        variantsArrowRight.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                variantsScrollView.smoothScrollBy(scrollSpeed, 0);
-            }
-        });
-
-        setVariants();
+        showPopup.run();
     }
 
     protected void setVariants()
