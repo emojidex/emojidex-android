b3f3c5dc3847057d6a7628afebcce2312276452a
kou <kou@genshin.org>
2017-11-16 16:24:44 +0900

    Use userEmoji API when logined premium or pro user.

--- EmojidexAndroidLibrary/src/main/java/com/emojidex/emojidexandroid/downloader/ExtendedJsonDownloadTask.java
+++ EmojidexAndroidLibrary/src/main/java/com/emojidex/emojidexandroid/downloader/ExtendedJsonDownloadTask.java
@@ -1,8 +1,10 @@
 package com.emojidex.emojidexandroid.downloader;
 
+import com.emojidex.emojidexandroid.EmojidexLocale;
 import com.emojidex.emojidexandroid.downloader.arguments.ExtendedDownloadArguments;
 import com.emojidex.libemojidex.Emojidex.Client;
 import com.emojidex.libemojidex.Emojidex.Data.Collection;
+import com.emojidex.libemojidex.Emojidex.Service.User;
 
 /**
  * Created by kou on 17/08/29.
@@ -29,6 +31,19 @@ class ExtendedJsonDownloadTask extends AbstractJsonDownloadTask{
     {
         final ExtendedDownloadArguments arguments = (ExtendedDownloadArguments)getArguments();
         final Client client = createEmojidexClient();
+        final User user = client.getUser();
+
+        // When logined and premium or pro user.
+        if(     user.getStatus().equals(User.AuthStatusCode.VERIFIED)
+            &&  (user.getPremium() || user.getPro())  )
+        {
+            final String target = arguments.getLocale().equals(EmojidexLocale.JAPANESE) ? "絵文字デックス" : "emojidex";
+            final Collection collection = client.getIndexes().userEmoji(target, 1, 0x7FFFFFFF, true);
+            while(collection.all().size() < collection.getTotal_count())
+                collection.more();
+            return collection;
+        }
+
         return client.getIndexes().extendedEmoji(arguments.getLocale().getLocale(), true);
     }
 }
